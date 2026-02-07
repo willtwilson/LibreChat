64b63a99c0aeed222db26698cb1058bea12c8cb4
Danny Avila <danny@librechat.ai>
2026-02-07 00:27:57 -0500

    chore: add runName to chainOptions in AgentClient

--- api/server/controllers/agents/client.js
+++ api/server/controllers/agents/client.js
@@ -1057,6 +1057,7 @@ class AgentClient extends BaseClient {
         titlePrompt: endpointConfig?.titlePrompt,
         titlePromptTemplate: endpointConfig?.titlePromptTemplate,
         chainOptions: {
+          runName: 'TitleRun',
           signal: abortController.signal,
           callbacks: [
             {
